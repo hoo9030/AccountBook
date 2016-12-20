41077003f3e8bd79877f239c3ce316562e15897e
hoo9030 <hoo9030@naver.com>
2016-12-20 18:03:11 +0900

    AccountBook First Commit

---
+++ SQLiteTest/src/com/example/sqlitetest/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.sqlitetest;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+	
+	final 
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		
+	}
+
+}
